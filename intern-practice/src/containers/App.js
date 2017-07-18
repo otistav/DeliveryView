f4bac92f935636ef2a0f083d159c5e22931b779c
otistav <semen_bard@mail.ru>
2017-07-18 16:31:48 +0300

    added comment in app.js

--- intern-practice/src/containers/App.js
+++ intern-practice/src/containers/App.js
@@ -17,12 +17,12 @@ class App extends Component {
 
   render() {
     return (
-        <div>
-          {/* <h1>{this.props.ItemState.length}</h1>*/}
-          <Header foodState={this.props.foodState} />
-          <ShopCard ItemsArray={this.props.ItemState} />
+      <div>
+        {/* <h1>{this.props.ItemState.length}</h1>*/}
+        <Header foodState={this.props.foodState} />
+        <ShopCard ItemsArray={this.props.ItemState} />
 
-        </div>
+      </div>
     );
   }
 }
@@ -32,45 +32,46 @@ export default connect(
       ItemState: state.itemList,
       foodState: state.beer,
     }),
-    dispatch => ({
-      onGetItems: () => {
-        const GetBeer = () => {
-          const transformBeerList = (array) => {
-            console.log('this is beerlist', array);
-            let i = -1;
-            const beerCostArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-              15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
-            return array.data.map((item) => {
-              i++;
-              return {
-                name: item.name,
-                description: item.tagline,
-                quantity: 0,
-                img: item.image_url,
-                state: false,
-                cost: beerCostArray[i],
-              };
-            });
-          };
-
-          return (dispatch) => {
-            dispatch({ type: 'FETCH_BEER_START' });
-            axios.get('https://api.punkapi.com/v2/beers')
-                            .then((response) => {
-                              response.data = transformBeerList(response);
-                              dispatch({ type: 'RECEIVE_BEER', payload: response.data });
-                            });
-
-
-                        //     setTimeout(() => {
-                        //         console.log('I got items');
-                        //         dispatch({ type: 'FETCH_TRACKS_SUCCESS', payload: [] });
-                        //     }, 2000)
-          };
+  dispatch => ({
+    onGetItems: () => {
+      const GetBeer = () => {
+          // transformBeerList is a func which adds a costs of beer in props object
+        const transformBeerList = (array) => {
+          console.log('this is beerlist', array);
+          let i = -1;
+          const beerCostArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+            15, 16, 17, 18, 19, 20, 21, 22, 23, 24];
+          return array.data.map((item) => {
+            i++;
+            return {
+              name: item.name,
+              description: item.tagline,
+              quantity: 0,
+              img: item.image_url,
+              state: false,
+              cost: beerCostArray[i],
+            };
+          });
         };
-        dispatch(GetBeer());
-      },
-    }),
+
+        return (dispatch) => {
+          dispatch({ type: 'FETCH_BEER_START' });
+          axios.get('https://api.punkapi.com/v2/beers')
+              .then((response) => {
+                response.data = transformBeerList(response);
+                dispatch({ type: 'RECEIVE_BEER', payload: response.data });
+              });
+
+
+            //     setTimeout(() => {
+            //         console.log('I got items');
+            //         dispatch({ type: 'FETCH_TRACKS_SUCCESS', payload: [] });
+            //     }, 2000)
+        };
+      };
+      dispatch(GetBeer());
+    },
+  }),
 
 
 )(App);
